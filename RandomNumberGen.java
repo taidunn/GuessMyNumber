d4dc2c991b463b4e40dbf0f3c14a554af29eaca2
Tai <macuser@Macs-MacBook-Pro-6.local>
2018-11-18 21:05:58 -0500

    Update RandomNumberGen.java

--- RandomNumberGen.java
+++ RandomNumberGen.java
@@ -8,7 +8,17 @@ public class RandomNumberGen {
 		{
 			int randomNumberEnt = userInput.nextInt();
 			if ((randomNumberEnt <= 100) && (randomNumberEnt > -1)) {
-				int generateRandom = (int)(Math.random()*101);
+				int max1115 = (int)(Math.random()*116);
+				/*The following if statement is to increase the amount of times
+				*Math.random will pick a number greater than or equal to 100.
+				*/
+				if (max1115 >= 100) {
+				//Any number greater than or equal to 100 will default to 100.
+					int generateRandom = 100;
+				}
+				else {
+					int generateRandom = max1115;
+				}
 				if (randomNumberEnt > generateRandom) {
 					System.out.println("You entered the number " + randomNumberEnt + " and the generated number was " + generateRandom + ".");
 					System.out.println("You guessed the highest number!");
