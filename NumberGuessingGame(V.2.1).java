a48d444f340c81d7bf27358aff274acbefecaa95
Tai <macuser@Macs-MacBook-Pro-6.local>
2018-11-29 10:45:09 -0500

    Intro Explanation
    Brings in Version 2.1 and it's explanation on how it is going to be different.

---
+++ NumberGuessingGame(V.2.1).java
@@ -0,0 +1,5 @@
+/*This is the newest version of the random number generator...
+* It is a much more simplified method. Doesn't use "if statements"
+* inside of more "if statments". Infact to be honest it uses no
+* if statement at all. However it does still use the while loop.
+*/
