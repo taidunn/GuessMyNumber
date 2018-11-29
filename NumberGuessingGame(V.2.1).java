cb31c8971db29b0d3e3652c81ab21cfa1ca6173c
Tai <macuser@Macs-MacBook-Pro-6.local>
2018-11-29 10:53:26 -0500

    Update NumberGuessingGame(V.2.1).java

--- NumberGuessingGame(V.2.1).java
+++ NumberGuessingGame(V.2.1).java
@@ -3,3 +3,12 @@
 * inside of more "if statments". Infact to be honest it uses no
 * if statement at all. However it does still use the while loop.
 */
+import java.util.Scanner;
+
+public class RandomNumberGen {
+  /*These public class names will need to be changed to whatever you decide
+  *to change the file name to. At this moment the download will not run this
+  *because the public class name does not match the file name. The file name was
+  *changed so anyone could tell the Version I was on.
+  */
+}
